869e96a61d9d202cf81774dd93bcbfbaf262e543
IT21161360 <99222052+IT21161360@users.noreply.github.com>
2022-02-23 04:36:33 +0000

    I have done my tutorial 3

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
-int main() {
+int main()
+
+ {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
- for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
+ 
+ cout <<setw(5) <<"No"<<setw(15)<< "Name"<<setw(10) << "Marks" << endl;
+ 
+ for (int r = 0; r < 5; r++) 
+ {
+ cout<<std::setw(5)<<r+1<<std::setw(15)<<names[r]<<std::setw(10)<<std::setiosflags(ios::fixed)<<std::setprecision(2)<< marks[r] << endl;
  }
+ 
 }
--- exercise04.cpp
+++ exercise04.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 
 // Do not change the main() function
-int main() {
-   int length = 10, width = 5;
+int main() 
+{
+   int length,width;
    input(length, width);
    print(length, width);
    return 0;
@@ -14,8 +14,16 @@ int main() {
 
 // Do not change the print() function
 void print(int len, int wth) {
-   cout << "Length : " << len 
-        << ", Width  : " << wth << endl;
+   cout << "Length : " << len << ", Width  : " << wth 
+   <<endl;
 }
 
 // Implement the Input Function here
+void input(int &len, int &wth)
+{
+    cout<<"Enter the length:"<<endl;
+    cin>>len;
+    cout<<"Enter the width :"<<endl;
+    cin>>wth;
+    
+}
